385648302447a0717c6b6b720483ef64f500b539
Yannis-Alouache <alouacehyannis1@gmail.com>
2023-03-19 15:48:57 +0100

    [+] added db port

--- src/services/Db.js
+++ src/services/Db.js
@@ -8,6 +8,7 @@ export class Db {
             host:       this.env.get("DB_HOST"),
             user:       this.env.get("DB_USER"),
             password:   this.env.get("DB_PASSWORD"),
+            port:       this.env.get("DB_PORT"),
             database:   this.env.get("DB_NAME")
         }).promise()
     }
