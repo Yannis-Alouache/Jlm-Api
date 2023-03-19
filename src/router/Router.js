a6fd0fa5a3a4b214a6c5fa345589f8e2f8e591d0
Yannis-Alouache <alouacehyannis1@gmail.com>
2023-03-19 14:38:22 +0100

    [+] changed cors rules

--- src/router/Router.js
+++ src/router/Router.js
@@ -11,7 +11,7 @@ export class Router {
         this.server = express()
 
         this.server.use(function(req, res, next) {
-            res.header("Access-Control-Allow-Origin", 'http://localhost:3000');
+            res.header("Access-Control-Allow-Origin", 'https://jlm-traitements.fr/');
             res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
             next();
         });
